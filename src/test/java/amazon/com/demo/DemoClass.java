615814f2a33967fdeaddb29f6acc91b5fa893f0a
manikandan <damumani96@gmail.com>
2026-04-06 19:59:16 +0530

    small allignment

--- src/test/java/amazon/com/demo/DemoClass.java
+++ src/test/java/amazon/com/demo/DemoClass.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class DemoClass {
 
 	public static void main(String[] args) {
-WebDriver d = new ChromeDriver();
+		WebDriver d = new ChromeDriver();
 		
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
