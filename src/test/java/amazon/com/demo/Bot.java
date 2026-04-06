02eabb04368df399d12f5b0b72fa30a936e7e95b
manikandan <damumani96@gmail.com>
2026-04-06 19:56:47 +0530

    New code added to launch browser in Bot.class file

--- src/test/java/amazon/com/demo/Bot.java
+++ src/test/java/amazon/com/demo/Bot.java
@@ -1,9 +1,22 @@
 package amazon.com.demo;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Bot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver d = new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+//		WebDriverWait wait = new WebDriverWait(d, Duration.ofSeconds(60));
+		d.get("https://testautomationpractice.blogspot.com/#");
+		
+		d.quit();
+
 
 	}
 
